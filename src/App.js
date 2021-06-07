c4ae2a9d5f0f2e5110bbc802fd5209c4d4ca6993
Rishabh Bhardwaj <rishabhrbs96@gmail.com>
2021-06-08 01:56:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Initial code for AnimePins.com
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
